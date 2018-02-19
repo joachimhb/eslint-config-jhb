fd45c7c4f3b4832eb2556e0225094bd17d1268c3
Joachim Haecker-Becker <j.hb@arcor.de>
2018-02-19 21:30:51 +0100

    license...

--- roboter.js
+++ roboter.js
@@ -11,17 +11,21 @@ roboter
         'MIT/X11',
         'ISC',
         'BSD',
+        'BSD*',
         'BSD-2-Clause',
         'BSD-3-Clause',
+        'BSD-like',
         'Apache-2.0',
         'Apache-2.0*',
         'LGPL-2.1+',
         'Public Domain',
 
-        // 'CC-BY-4.0',
+        'CC-BY-4.0',
+        '(MIT OR Apache-2.0)',
 
-        // combinations
-        // '(MIT AND CC-BY-3.0)',
+        '(MIT AND JSON)',
+
+        '(MIT AND CC-BY-3.0)',
         'BSD-3-Clause OR MIT',
         '(WTFPL OR MIT)',
         '(GPL-2.0 OR MIT)',
